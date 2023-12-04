13f2b43b0aa56f17ad17f22239557be6ae1e7e93
Ethan Shafer <ethan.h.shafer@gmail.com>
2023-12-04 00:06:14 -0500

    Part 1

--- day4.py
+++ day4.py
@@ -7,6 +7,21 @@ def day4_part1(is_test: bool=True):
         file = "data/day4.txt"
 
     all_lines = read_file(file)
+    values = []
+    for line in all_lines:
+        winning_numbers, my_numbers = line.split(': ')[1].split(' | ')
+        winning_numbers = set(winning_numbers.split())
+        my_numbers = set(my_numbers.split())
+        intersect = winning_numbers.intersection(my_numbers)
+
+        if len(intersect) != 0:
+            value = 2 ** (len(intersect) - 1)
+        else:
+            value = 0
+        values.append(value)
+    return sum(values)
+
+        
     
     
 def day4_part2(is_test: bool=True):
@@ -18,4 +33,4 @@ def day4_part2(is_test: bool=True):
     all_lines = read_file(file)
 
 
-print(day4_part1())
+print(day4_part1(False))
