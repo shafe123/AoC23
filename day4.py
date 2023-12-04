81869857090fa96edb1d343eea1cd0b69a676b62
Ethan Shafer <ethan.h.shafer@gmail.com>
2023-12-04 00:29:19 -0500

    Part 2 with tests

--- day4.py
+++ day4.py
@@ -21,9 +21,7 @@ def day4_part1(is_test: bool=True):
         values.append(value)
     return sum(values)
 
-        
-    
-    
+
 def day4_part2(is_test: bool=True):
     if is_test:
         file = "data/day4_sample.txt"
@@ -31,6 +29,21 @@ def day4_part2(is_test: bool=True):
         file = "data/day4.txt"
 
     all_lines = read_file(file)
+    to_process = list(range(1, len(all_lines) + 1))
+    counts = [1] * len(all_lines)
+
+    for card_num in range(len(all_lines)):
+        winning_numbers, my_numbers = all_lines[card_num].split(': ')[1].split(' | ')
+        winning_numbers = set(winning_numbers.split())
+        my_numbers = set(my_numbers.split())
+
+        for num in range(card_num + 1, card_num + 1 + len(winning_numbers.intersection(my_numbers))):
+            if num >= len(all_lines):
+                break
+            counts[num] += counts[card_num]
+
+    return sum(counts)
 
 
 print(day4_part1(False))
+print(day4_part2(False))
--- tests/test_day4.py
+++ tests/test_day4.py
@@ -3,5 +3,5 @@ import day4
 
 class test_day4(unittest.TestCase):
     def test_integration(self):
-        self.assertEqual(day4.day4_part1(), 8)
-        # self.assertEqual(day4.day4_part2(), 2286)
+        self.assertEqual(day4.day4_part1(), 13)
+        self.assertEqual(day4.day4_part2(), 30)
