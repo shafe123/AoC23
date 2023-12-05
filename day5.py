16f07f71cbbc6726eaba359a3ddf9ace2a577564
Ethan Shafer <ethan.h.shafer@gmail.com>
2023-12-05 00:50:29 -0500

    Part 1

--- day5.py
+++ day5.py
@@ -1,4 +1,5 @@
 from utilities import read_file
+import math
 
 def part1(is_test: bool=True):
     if is_test:
@@ -7,7 +8,38 @@ def part1(is_test: bool=True):
         file = "data/day5.txt"
 
     all_lines = read_file(file)
+    seeds, maps = clean_lines(all_lines)
     
+    steps = []
+    locations = []
+    for seed in seeds:
+        current_val = seed
+        steps.append([current_val])
+
+        for map in maps:
+            for destination, source, length in map:
+                if source <= current_val < source + length:
+                    current_val = destination + current_val - source
+                    break
+            steps[-1].append(current_val)
+
+        locations.append(current_val)
+
+    return min(locations)
+
+def clean_lines(all_lines):
+    maps = []
+    seeds = [int(x) for x in all_lines[0].split(': ')[1].split()]
+    for line in all_lines[1:]:
+        if line.strip() == '':
+            maps.append([])
+        elif line.strip().replace(' ', '').isnumeric():
+            maps[-1].append([int(x) for x in line.split()])
+        else:
+            continue
+    return seeds, maps
+
+print(part1(False))
     
 
 def part2(is_test: bool=True):
@@ -20,5 +52,5 @@ def part2(is_test: bool=True):
     
 
 
-print(part1())
+
 #print(part2())
--- tests/test_day5.py
+++ tests/test_day5.py
@@ -3,5 +3,5 @@ import day5
 
 class test_day5(unittest.TestCase):
     def test_integration(self):
-        self.assertEqual(day5.part1(), None)
+        self.assertEqual(day5.part1(), 35)
         self.assertEqual(day5.part2(), None)
