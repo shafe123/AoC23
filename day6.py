4abf7e240f29095f7b9a6a115b896af67e6e4573
Ethan Shafer <ethan.h.shafer@gmail.com>
2023-12-06 00:12:55 -0500

    Day 6 part 1

--- day6.py
+++ day6.py
@@ -1,4 +1,5 @@
 from utilities import read_file
+import math
 
 def part1(is_test: bool=True):
     if is_test:
@@ -7,6 +8,56 @@ def part1(is_test: bool=True):
         file = "data/day6.txt"
 
     all_lines = read_file(file)
+    times = [int(x) for x in all_lines[0][10:].split()]
+    distances = [int(x) for x in all_lines[1][10:].split()]
 
+    races = zip(times, distances)
 
-print(part1())
+    all_wins = []
+    for time, record in races:
+        possible_wins = []
+
+        start_point_left = math.floor(time / 2)
+        start_point_right = math.ceil(time / 2)
+
+        if start_point_left == start_point_right:
+            start_point_right += 1
+
+        initial_left_distance = calculate_distance(start_point_left, time)
+        while initial_left_distance > record:
+            possible_wins.append(initial_left_distance)
+            start_point_left -= 1
+            initial_left_distance = calculate_distance(start_point_left, time)
+
+        initial_right_distance = calculate_distance(start_point_right, time)
+        while initial_right_distance > record:
+            possible_wins.append(initial_right_distance)
+            start_point_right += 1
+            initial_right_distance = calculate_distance(start_point_right, time)
+
+        all_wins.append(len(possible_wins))
+    
+    product = 1
+    for counts in all_wins:
+        product *= counts
+    return product
+
+        
+def calculate_distance(velocity, time):
+    time_remaining = time - velocity
+    distance = velocity * time_remaining
+    return distance
+
+# print(part1(False))
+
+def part2(is_test: bool=True):
+    if is_test:
+        file = "data/day6_sample.txt"
+    else:
+        file = "data/day6.txt"
+
+    all_lines = read_file(file)
+    times = [int(x) for x in all_lines[0][10:].split()]
+    distances = [int(x) for x in all_lines[1][10:].split()]
+
+print(part2())
--- tests/test_day6.py
+++ tests/test_day6.py
@@ -1,6 +1,7 @@
 import unittest
 import day6
 
-class test_day5(unittest.TestCase):
+class test_day6(unittest.TestCase):
     def test_integration(self):
-        self.assertEqual(day6.part1(), 35)
+        self.assertEqual(day6.part1(), 288)
+        self.assertEqual(day6.part2(), None)
