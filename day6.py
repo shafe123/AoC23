bc592be76f4407147f134d6a0089848120c2e799
Ethan Shafer <ethan.h.shafer@gmail.com>
2023-12-06 00:20:29 -0500

    Day 6 part 2

--- day6.py
+++ day6.py
@@ -57,7 +57,25 @@ def part2(is_test: bool=True):
         file = "data/day6.txt"
 
     all_lines = read_file(file)
-    times = [int(x) for x in all_lines[0][10:].split()]
-    distances = [int(x) for x in all_lines[1][10:].split()]
+    time = int(all_lines[0][10:].replace(' ', ''))
+    record = int(all_lines[1][10:].replace(' ', ''))
+    print(time, record)
 
-print(part2())
+    possible_wins = []
+
+    start_point_left = math.floor(time / 2)
+    start_point_right = math.ceil(time / 2)
+
+    if start_point_left == start_point_right:
+        start_point_right += 1
+
+    initial_left_distance = calculate_distance(start_point_left, time)
+    while initial_left_distance > record:
+        possible_wins.append(initial_left_distance)
+        start_point_left -= 1
+        initial_left_distance = calculate_distance(start_point_left, time)
+    
+    return len(possible_wins) * 2 if start_point_left != start_point_right else len(possible_wins) * 2 + 1
+
+
+print(part2(False))
--- tests/test_day6.py
+++ tests/test_day6.py
@@ -4,4 +4,4 @@ import day6
 class test_day6(unittest.TestCase):
     def test_integration(self):
         self.assertEqual(day6.part1(), 288)
-        self.assertEqual(day6.part2(), None)
+        self.assertEqual(day6.part2(), 71504)
