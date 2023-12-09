f0439979923c4b83ddb38401fa6987c11b76e9f5
shafe123 <ethan.shafer@westpoint.edu>
2023-12-09 00:26:23 -0500

    Day 9 part 2

--- day9.py
+++ day9.py
@@ -13,13 +13,7 @@ def part1(is_test: bool = True):
 
     predictions = []
     for line in all_lines:
-        differences = [line.copy()]
-        while any(x != 0 for x in differences[-1]):
-            new_line = []
-            for index, val in enumerate(differences[-1][:-1]):
-                new_line.append(differences[-1][index + 1] - val)
-            differences.append(new_line)
-        differences[-1].append(0)
+        differences = find_diff_chain(line)
 
         for index in range(len(differences) - 2, -1, -1):
             new_val = differences[index + 1][-1] + differences[index][-1]
@@ -29,6 +23,32 @@ def part1(is_test: bool = True):
 
     return sum(predictions)
 
-
+def find_diff_chain(line: list[int]) -> list[list[int]]:
+    differences = [line.copy()]
+    while any(x != 0 for x in differences[-1]):
+        new_line = []
+        for index, val in enumerate(differences[-1][:-1]):
+            new_line.append(differences[-1][index + 1] - val)
+        differences.append(new_line)
+    differences[-1].append(0)
+    return differences
 
 print(part1(False))
+
+def part2(is_test: bool = True):
+    all_lines = get_lines(9, is_test)
+    all_lines = clean_lines(all_lines)
+
+    predictions = []
+    for line in all_lines:
+        differences = find_diff_chain(line)
+
+        for index in range(len(differences) - 2, -1, -1):
+            new_val = differences[index][0] - differences[index + 1][0]
+            differences[index].insert(0, new_val)
+    
+        predictions.append(differences[0][0])
+
+    return sum(predictions)
+
+print(part2(False))
--- tests/test_day9.py
+++ tests/test_day9.py
@@ -4,3 +4,4 @@ from day9 import *
 class test_day9(unittest.TestCase):
     def integration_test(self):
         self.assertEqual(part1(), 114)
+        self.assertEqual(part1(), 2)
