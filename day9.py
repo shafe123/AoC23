fdedebc8909bec8dac8495d5aff0fa062761160c
shafe123 <ethan.shafer@westpoint.edu>
2023-12-08 23:24:19 -0500

    Day 9 prep

---
+++ day9.py
@@ -0,0 +1,8 @@
+from utilities import get_lines
+
+
+def part1(is_test: bool = True):
+    all_lines = get_lines(9, is_test)
+
+
+print(part1())
---
+++ tests/test_day9.py
@@ -0,0 +1,6 @@
+import unittest
+from day9 import *
+
+class test_day9(unittest.TestCase):
+    def integration_test(self):
+        self.assertEqual(part1(False), None)
