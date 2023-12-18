815e300c5d72b752b863f7c61c88f69c231a5316
shafe123 <ethan.shafer@westpoint.edu>
2023-12-18 15:46:03 -0500

    Day 18 part 2 - learning Pick's Theorem & Shoelace Formula

--- day18.py
+++ day18.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 from heapq import heappop, heappush
 import copy
 import math
+import itertools
 
 
 def mark_interior(start_point, grid, mark_character="*", guard_character="#"):
@@ -98,9 +99,62 @@ def part1(is_test: bool = True):
 
     # I looked at my grid...
     new_grid = mark_interior((183, 1), grid)
-    print_grid(new_grid)
+    # print_grid(new_grid)
     return grid_string(new_grid).count("*") + len(main_loop) - 1
 
 
+# if __name__ == "__main__":
+#     print(part1(False))
+
+
+def part2(is_test: bool = True):
+    all_lines = get_lines(18, is_test)
+
+    current_pos = (0, 0)
+    directions = []
+    nodes = []
+    for line in all_lines:
+        direction, distance, color = line.split()
+
+        distance = int(color[2:7], base=16)
+        direction = color[-2]
+        mapping = { '0': 'R', '1': 'D', '2': 'L', '3': 'U'}
+        direction = mapping[direction]
+
+        directions.append((direction, distance, color))
+
+        match direction:
+            case "R":
+                current_pos = add_tuples(current_pos, (0, distance))
+            case "L":
+                current_pos = add_tuples(current_pos, (0, -distance))
+            case "U":
+                current_pos = add_tuples(current_pos, (-distance, 0))
+            case "D":
+                current_pos = add_tuples(current_pos, (distance, 0))
+        nodes.append(current_pos)
+
+    # shoelace formula
+    return picks(nodes)
+
+def picks(nodes):
+    A = shoelace(nodes)
+    p = perimeter(nodes)
+    return A + p / 2 + 1
+
+def perimeter(nodes):
+    perimeter = max(nodes[0][0], nodes[0][1])
+    for (x0, y0), (x1, y1) in itertools.pairwise(nodes):
+        perimeter += abs(x0 - x1) + abs(y0 - y1)
+    return perimeter
+
+def shoelace(nodes):
+    total = 0
+    for (x0, y0), (x1, y1) in itertools.pairwise(nodes):
+        total += (y0 + y1) * (x0 - x1)
+    total += (nodes[-1][0] + nodes[0][0]) * (nodes[-1][1] - nodes[0][1])
+    return abs(total / 2)
+    
+
 if __name__ == "__main__":
-    print(part1(False))
+    print(part2(False))
